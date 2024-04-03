0504c0d5c608e90b82b6979c0f0581e6356a9e40
Sir Ronney Otieno <ronvi035@gmail.com>
2024-04-03 13:52:36 +0300

    add components

---
+++ src/sections/index.js
@@ -0,0 +1,30 @@
+import Hero from './Hero.jsx'
+import PopularProducts from './PopularProducts.jsx'
+import SuperQuality from './SuperQuality.jsx'
+import Services from './Services.jsx'
+import SpecialOffer from './SpecialOffer.jsx'
+import Subscribe from './Subscribe.jsx'
+import CustomerReviews from './CustomerReviews.jsx'
+import Footer from './Footer.jsx'
+
+
+export {
+    Hero,
+    PopularProducts,
+    SuperQuality,
+    Services,
+    SpecialOffer,
+    Subscribe,
+    CustomerReviews,
+    Footer
+
+}
+
+
+
+
+
+
+
+
+
